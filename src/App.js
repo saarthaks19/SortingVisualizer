0f98a71d6ee09fdca69b306188d9fd72703b6ff7
Saarthak Sharma <saarthaksharma@Saarthaks-MacBook-Air.local>
2024-08-08 21:34:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <SortingVisualizer />
+        </div>
+    );
+}
+
+export default App;
